9d5872bdb0b262fa5a8fb7e1a796a30ac0e6ed6a
Amanpreet Singh <apsdehal@gmail.com>
2014-08-07 21:26:42 +0530

    Updates many things

--- Pundit/bookmarklet_build/build_bookmarklet.sh
+++ Pundit/bookmarklet_build/build_bookmarklet.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sdk=../../dojo_sdk_1.6.1
+sdk=../dojo
 ver=`head -1 ../VERSION.txt`
 
 # Path where to find 
@@ -14,25 +14,25 @@ bmpath=https://rawgit.com/apsdehal/WikidataAnnotationFeeder/master/Pundit
 ### Dont touch anything below this line ###
 ###########################################
 
-pwd=`pwd`
+currentpwd=`pwd`
 
-echo "Building Pundit ${ver} Bookmarklet in ${pwd}"
-echo "Will load itself from ${bmpath}"
-echo .
+# echo "Building Pundit ${ver} Bookmarklet in ${currentpwd}"
+# echo "Will load itself from ${bmpath}"
+# echo .
 
-rm -rf ${pwd}/dojo
+# rm -rf ${currentpwd}/dojo
 
-cd ${sdk}/util/buildscripts
-./build.sh action=release profileFile=${pwd}/bookmarklet.profile.js loader=xdomain version=${ver} releaseDir=${pwd} cssOptimize=comments.keepLines optimize=shrinksafe.keepLines
-cd ${pwd}
-mv dojo foo
-mkdir dojo
-mv foo/dojo/dojo.xd.js dojo
-mv foo/dojo/Bookmarklet.xd.js dojo
-mkdir dojo/nls
-mv foo/dojo/nls/Bookmarklet*.xd.js dojo/nls
-mv foo/dojo/resources dojo
-rm -rf foo
+# cd ${sdk}/util/buildscripts
+# ./build.sh action=release profileFile=${currentpwd}/bookmarklet.profile.js loader=xdomain version=${ver} releaseDir=${currentpwd} cssOptimize=comments.keepLines optimize=shrinksafe.keepLines
+# cd ${currentpwd}
+# mv dojo foo
+# mkdir dojo
+# mv foo/dojo/dojo.xd.js dojo
+# mv foo/dojo/Bookmarklet.xd.js dojo
+# mkdir dojo/nls
+# mv foo/dojo/nls/Bookmarklet*.xd.js dojo/nls
+# mv foo/dojo/resources dojo
+# rm -rf foo
 
 echo "Preparing InitBookmarklet.js for ${bmpath}"
 sed "s%{bm-abs-path}%${bmpath}%g" InitBookmarklet.js-template > ../src/InitBookmarklet.js
@@ -44,4 +44,4 @@ echo .
 
 
 echo .
-echo "Pundit bookmarklet build is ready at ${pwd}/dojo"
+echo "Wikidata Web Annotator bookmarklet build is ready at ${currentpwd}/dojo"
